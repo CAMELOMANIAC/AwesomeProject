d64197512b86b6e4272363518a63356ed98c8508
yugyusang <yugyusang12@gmail.com>
2024-03-29 23:58:44 +0900

    리코일 설치

--- App.tsx
+++ App.tsx
@@ -5,32 +5,35 @@
  * @format
  */
 
-import {NavigationContainer, NavigationProp} from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import Notice from './components/page/Notice';
 import Home from './components/page/Home';
 import Test from './components/page/Test';
 import Crawling from './components/page/Crawling';
+import {RecoilRoot} from 'recoil';
 
 function App(): React.JSX.Element {
     const Stack = createNativeStackNavigator();
 
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home">
-                    {props => <Home {...props} extraData={123} />}
-                </Stack.Screen>
-                <Stack.Screen
-                    name="Notice"
-                    component={Notice}
-                    initialParams={{itemId: '처음에만되나?'}}
-                />
-                <Stack.Screen name="Test" component={Test} />
-                <Stack.Screen name="Crawling" component={Crawling} />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <RecoilRoot>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="Home">
+                        {props => <Home {...props} extraData={123} />}
+                    </Stack.Screen>
+                    <Stack.Screen
+                        name="Notice"
+                        component={Notice}
+                        initialParams={{itemId: '처음에만되나?'}}
+                    />
+                    <Stack.Screen name="Test" component={Test} />
+                    <Stack.Screen name="Crawling" component={Crawling} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </RecoilRoot>
     );
 }
 
