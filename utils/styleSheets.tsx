695e42d655b42929dd8dfff11266397b55a4751f
yugyusang <yugyusang12@gmail.com>
2024-04-09 00:06:32 +0900

    카드 스타일시트 추가

--- utils/styleSheets.tsx
+++ utils/styleSheets.tsx
@@ -1,6 +1,20 @@
 import {StyleSheet} from 'react-native';
+import {Dimensions} from 'react-native';
+
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
 
 export const styles = StyleSheet.create({
     screen: {flex: 1, alignItems: 'center', justifyContent: 'center'},
     checkBox: {flexDirection: 'row', alignItems: 'center'},
+    card: {
+        position: 'absolute',
+        backgroundColor: 'gray',
+        width: screenWidth - 40,
+        height: screenHeight - 40,
+        borderRadius: 10,
+        padding: 10,
+        margin: 10,
+        justifyContent: 'flex-end',
+    },
 });
