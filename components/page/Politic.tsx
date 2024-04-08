e19fe5e226a8caa46d828d28339a40ffa1c2d2a6
yugyusang <yugyusang12@gmail.com>
2024-04-09 00:08:33 +0900

    정책 스크린컴포넌트가 정책카드컴포넌트를 렌더링하는 기능 추가

--- components/page/Politic.tsx
+++ components/page/Politic.tsx
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View} from 'react-native';
-
+import {PartyType} from './Test';
+import {styles} from '../../utils/styleSheets';
+import PolicyCard from '../PolicyCard';
 type props = {
     route?: {
         params: {
             checkedPrms: string[];
+            partyArray: Array<PartyType>;
         };
     };
 };
 const Politic = ({route}: props) => {
-    console.log(route?.params.checkedPrms);
+    const [partyArray, setPartyArray] = useState<Array<PartyType>>([]);
+    useEffect(() => {
+        const filterdparty = route?.params.partyArray.filter(party =>
+            route?.params.checkedPrms.some(checkedPrm =>
+                party.prmsArray.some(prms =>
+                    prms.prmsRealmName.includes(checkedPrm),
+                ),
+            ),
+        );
+        filterdparty?.forEach(party => {
+            setPartyArray(prev => [
+                ...prev,
+                {
+                    partyName: party.partyName,
+                    prmsArray: party.prmsArray.filter(prms =>
+                        route?.params.checkedPrms.some(checkedPrm =>
+                            prms.prmsRealmName.includes(checkedPrm),
+                        ),
+                    ),
+                },
+            ]);
+        });
+    }, []);
     return (
-        <View>
+        <View style={styles.screen}>
             {route?.params.checkedPrms.map(item => (
-                <Text>{item}</Text>
+                <Text key={item}>{item}</Text>
+            ))}
+            {partyArray.map(party => (
+                <PolicyCard key={party.partyName} party={party} />
             ))}
         </View>
     );
