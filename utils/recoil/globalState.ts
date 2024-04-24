2378513dec669fa2d8ffcc76a9685752c8830699
yugyusang <yugyusang12@gmail.com>
2024-04-24 23:12:11 +0900

    revealCount상태 추가

--- utils/recoil/globalState.ts
+++ utils/recoil/globalState.ts
@@ -1,12 +1,17 @@
 import {atom} from 'recoil';
-import {PartyType} from '../../components/page/Test';
+import {SplitPartyType} from '../../components/page/Politic';
 
 export const partyNameState = atom<Array<string>>({
     key: 'partyNameState',
     default: [],
 });
 
-export const likeArrayState = atom<Array<PartyType>>({
+export const likeArrayState = atom<Array<SplitPartyType>>({
     key: 'likeArrayState',
     default: [],
+});
+
+export const revealCountState = atom<number>({
+    key: 'revealCountState',
+    default: 0,
 });
