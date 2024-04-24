db28b82a9571d7ffac38869f591b32b96c05b06c
yugyusang <yugyusang12@gmail.com>
2024-04-24 23:10:00 +0900

    카드 애니메이션 수정

--- components/PolicyCard.tsx
+++ components/PolicyCard.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {
     PanGestureHandler,
     PanGestureHandlerGestureEvent,
+    ScrollView,
     State,
     TapGestureHandler,
     TapGestureHandlerStateChangeEvent,
@@ -11,21 +12,18 @@ import Animated, {
     useDerivedValue,
     useSharedValue,
     withSpring,
+    withTiming,
 } from 'react-native-reanimated';
-import {PartyType} from './page/Test';
 import {styles} from '../utils/styleSheets';
 import {Text, View} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
-import {StackNavigationProp} from '@react-navigation/stack';
-import {RootStackParamList} from '../App';
 import {useRecoilState} from 'recoil';
-import {likeArrayState} from '../utils/recoil/globalState';
+import {likeArrayState, revealCountState} from '../utils/recoil/globalState';
+import {SplitPartyType} from './page/Politic';
 
 const PolicyCard = ({
     party,
-    currentPolicy,
 }: {
-    party: PartyType;
+    party: SplitPartyType;
     currentPolicy: number;
 }) => {
     const translateX = useSharedValue(0);
@@ -35,8 +33,7 @@ const PolicyCard = ({
     const [isReveal, setIsReveal] = useState<boolean>(false);
     const [_likeArray, setLikeArray] = useRecoilState(likeArrayState);
     const [isPressed, setIsPressed] = useState<boolean>(false);
-
-    const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
+    const [_revealCount, setRevealCount] = useRecoilState(revealCountState);
 
     const rotateY = useDerivedValue(() => {
         if (isPressed) {
@@ -51,51 +48,74 @@ const PolicyCard = ({
             transform: [{rotateY: `${rotateY.value}deg`}, {perspective: 1000}],
         };
     });
-    const animatedStyles = useAnimatedStyle(() => {
+
+    const revealX = useDerivedValue(() => {
         if (isReveal) {
-            return {
-                transform: [
-                    {translateX: withSpring(rotate.value > 0 ? 500 : -500)},
-                    {translateY: translateY.value},
-                    {rotate: `${rotate.value}deg`},
-                    {rotateY: `${rotateY.value}deg`},
-                ],
-            };
-        } else {
-            return {
-                transform: [
-                    {translateX: translateX.value},
-                    {translateY: translateY.value},
-                    {rotate: `${rotate.value}deg`},
-                    {rotateY: `${rotateY.value}deg`},
-                ],
-            };
-        }
+            return withSpring(rotate.value > 0 ? 500 : -500);
+        } else return translateX.value;
     });
-    const likeTextStyles = useAnimatedStyle(() => {
+    const animatedStyles = useAnimatedStyle(() => {
         return {
-            position: 'absolute',
-            top: 20,
-            left: 20,
-            transform: [{rotate: `-30deg`}],
-            fontSize: 40,
-            color:
-                rotate.value < 0
-                    ? `rgba(0,0,0,0)`
-                    : `rgba(124,252,0,${Math.abs(rotate.value)})`,
+            transform: [
+                {translateX: revealX.value},
+                {translateY: translateY.value},
+                {rotate: `${rotate.value}deg`},
+                {rotateY: `${rotateY.value}deg`},
+            ],
         };
     });
+
+    const isFlipedShared = useSharedValue(isFliped);
+    useEffect(() => {
+        isFlipedShared.value = isFliped;
+    }, [isFliped]);
+
+    const commonStyles: {
+        justifyContent: 'center';
+        padding: number;
+        paddingLeft: number;
+        top: number;
+        borderWidth: number;
+        fontSize: number;
+        fontWeight: 'bold';
+        margin: number;
+    } = {
+        justifyContent: 'center',
+        padding: 2,
+        paddingLeft: 10,
+        top: 20,
+        borderWidth: 4,
+        fontSize: 40,
+        fontWeight: 'bold',
+        margin: 10,
+    };
+
+    const revealStandard = 5;
+    const likeTextStyles = useAnimatedStyle(() => {
+        return {
+            ...commonStyles,
+            borderColor: 'lime',
+            transform: [
+                {rotate: isFlipedShared.value ? `15deg` : `-15deg`},
+                {rotateY: `${rotateY.value}deg`},
+            ],
+            opacity:
+                rotate.value > 0 ? Math.abs(rotate.value) / revealStandard : 0,
+            color: 'lime',
+        };
+    });
+
     const nopeTextStyles = useAnimatedStyle(() => {
         return {
-            position: 'absolute',
-            top: 20,
-            right: 20,
-            transform: [{rotate: `30deg`}],
-            fontSize: 40,
-            color:
-                rotate.value < 0
-                    ? `rgba(255,0,0,${Math.abs(rotate.value)})`
-                    : `rgba(0,0,0,0)`,
+            ...commonStyles,
+            borderColor: 'red',
+            transform: [
+                {rotate: isFlipedShared.value ? `-15deg` : `15deg`},
+                {rotateY: `${rotateY.value}deg`},
+            ],
+            opacity:
+                rotate.value < 0 ? Math.abs(rotate.value) / revealStandard : 0,
+            color: 'red',
         };
     });
 
@@ -104,26 +124,36 @@ const PolicyCard = ({
             setIsPressed(true);
             translateX.value = event.nativeEvent.translationX;
             translateY.value = event.nativeEvent.translationY;
-            rotate.value = event.nativeEvent.translationX / 100;
+            rotate.value = event.nativeEvent.translationX / 20;
         }
     };
     const onEnd = (event: PanGestureHandlerGestureEvent) => {
         if (event.nativeEvent.state === State.END) {
             setIsPressed(false);
-            if (rotate.value > 1) {
+            if (rotate.value > revealStandard) {
                 setIsReveal(true);
-                currentPolicy === 0 && navigation.navigate('Info');
-            } else if (rotate.value < -1) {
+                setTimeout(() => {
+                    setLikeArray(prev => [...prev, party]);
+                    setRevealCount(prev => prev + 1);
+                    /*setLikeArray(prev => [...prev, party]);가 포함되면서 애니메이션이 늦게 동작하는 이유는 JavaScript의 이벤트 루프와 관련이 있을 수 있습니다.
+JavaScript는 단일 스레드 언어이므로, 한 번에 하나의 작업만 처리할 수 있습니다. 따라서, setLikeArray(prev => [...prev, party]);와 같은 상태 업데이트 작업이 이벤트 큐에 추가되면, JavaScript는 현재 실행 중인 모든 동기적인 작업이 완료될 때까지 이 작업을 대기시킵니다.
+이 경우, setLikeArray(prev => [...prev, party]);가 호출되면, 이 상태 업데이트는 이벤트 큐에 추가됩니다. 그런 다음, JavaScript는 현재 실행 중인 모든 동기적인 작업(예: 애니메이션)이 완료될 때까지 이 상태 업데이트를 대기시킵니다. 이로 인해 애니메이션이 늦게 시작될 수 있습니다.
+이 문제를 해결하려면, setLikeArray(prev => [...prev, party]);를 setTimeout 내부에 넣어 이벤트 루프의 다음 틱에서 실행되도록 할 수 있습니다. 이렇게 하면, 현재 실행 중인 동기적인 작업이 완료된 후에 상태 업데이트가 실행됩니다. 
+리액트는 최적화를 위해 별도의 이벤트큐를 이용해서 한꺼번에 렌더링함 자바스크립트상에서는 비동기적으로 하나씩 처리되어있지만 렌더링에서는 한꺼번에 처리되므로 setLikeArray와 setIsReveal가 동시에 실행된것처럼 보임 그러므로 일부러 실행될 틱을 나눠야 자연스럽게 보임*/
+                }, 0);
+            } else if (rotate.value < -revealStandard) {
                 setIsReveal(true);
-                setLikeArray(prev => [...prev, party]);
-                currentPolicy === 0 && navigation.navigate('Info');
+                setTimeout(() => {
+                    setRevealCount(prev => prev + 1);
+                }, 0);
             } else {
-                translateX.value = withSpring(0);
-                translateY.value = withSpring(0);
-                rotate.value = withSpring(0);
+                translateX.value = withTiming(0, {duration: 500});
+                translateY.value = withTiming(0, {duration: 500});
+                rotate.value = withTiming(0, {duration: 500});
             }
         }
     };
+
     const onEndTap = (event: TapGestureHandlerStateChangeEvent) => {
         if (event.nativeEvent.state === State.END) {
             setIsFliped(prev => !prev);
@@ -138,19 +168,45 @@ const PolicyCard = ({
                 onHandlerStateChange={onEnd}>
                 <Animated.View
                     style={[styles.card, animatedStyles, animatedFlipStyles]}>
-                    <Animated.View style={[styles.cardContent]}>
-                        {party.prmsArray.map(prms => (
-                            <View key={prms.prmsTitle}>
-                                <Text style={styles.cardBackcontent}>
-                                    {isFliped && prms.prmmCont}
-                                </Text>
-                                <Text>{!isFliped && prms.prmsTitle}</Text>
-                            </View>
-                        ))}
-                    </Animated.View>
-
-                    <Animated.Text style={[likeTextStyles]}>LIKE</Animated.Text>
-                    <Animated.Text style={[nopeTextStyles]}>NOPE</Animated.Text>
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            width: '100%',
+                            justifyContent: 'space-between',
+                            position: 'absolute',
+                        }}>
+                        {!isFliped ? (
+                            <>
+                                <Animated.Text style={[likeTextStyles]}>
+                                    LIKE
+                                </Animated.Text>
+                                <Animated.Text style={[nopeTextStyles]}>
+                                    NOPE
+                                </Animated.Text>
+                            </>
+                        ) : (
+                            <>
+                                <Animated.Text style={[nopeTextStyles]}>
+                                    NOPE
+                                </Animated.Text>
+                                <Animated.Text style={[likeTextStyles]}>
+                                    LIKE
+                                </Animated.Text>
+                            </>
+                        )}
+                    </View>
+                    {isFliped && (
+                        <ScrollView key={party.prmsTitle}>
+                            <Text style={styles.cardBackcontent}>
+                                {party.prmsContent}
+                            </Text>
+                        </ScrollView>
+                    )}
+                    {!isFliped && (
+                        <View style={[styles.cardContent]}>
+                            <Text>{party.prmsTitle}</Text>
+                        </View>
+                    )}
                 </Animated.View>
             </PanGestureHandler>
         </TapGestureHandler>
